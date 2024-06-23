0b7ca7adc0c8c6432c498166e442ed2b27472a4a
marshal <1337marshal1337@gmail.com>
2024-06-23 03:14:47 +0300

    Hero update

--- gigaprog_practice/src/Components/Hero.tsx
+++ gigaprog_practice/src/Components/Hero.tsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React from "react";
 import "../App.css";
 
 import arrowDown from "./Images/arrow_down.png";
 
 function Hero() {
   return (
-    <div className="Hero bg-hero">
-        <div className="text-[#252733]">
-          <h1 className="text-[48px] font-bold leading-[64px]">
-            Логістика, що рухає Україну вперед.
-          </h1>
-          <p className="text-[20px] leading-[30px]">
-            Відправте ваші вантажі із впевненістю – ми забезпечимо точне та
-            ефективне перевезення в межах всієї України.
-          </p>
-          <button className="w-[433px] h-[50px] bg-[#152892] rounded-[10px] font-semibold text-white">Замовити перевезення</button>
-          <img src={arrowDown} alt=""></img>
-        </div>
+    <div className="Hero px-[320px] pb-[60px] pt-[100px] bg-slate-400">
+      <div className="text-[#252733] text-center flex items-center flex-col">
+        <h1 className="text-[48px] font-bold leading-[64px] mb-[16px]">
+          Логістика, що рухає Україну <br /> вперед.
+        </h1>
+
+        <p className="text-[20px] leading-[30px] mb-[65px]">
+          Відправте ваші вантажі із впевненістю – ми забезпечимо точне та <br/>
+          ефективне перевезення в межах всієї України.
+        </p>
+
+        <button className="w-[433px] h-[50px] bg-[#152892] rounded-[10px] font-semibold text-white mb-[180px]">
+          Замовити перевезення
+        </button>
+        <img src={arrowDown} alt=""></img>
       </div>
-  )
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
